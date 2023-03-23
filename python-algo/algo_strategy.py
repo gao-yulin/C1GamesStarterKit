188faa8629084437601c8d164feb986c741b4795
Yulin Gao <yulingaoh@gmail.com>
2023-03-22 23:35:13 -0400

    Override start function

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -4,7 +4,7 @@ import math
 import warnings
 from sys import maxsize
 import json
-
+from gamelib.util import get_command, debug_write, BANNER_TEXT
 
 """
 Most of the algo code you write will be in this file unless you create new
@@ -59,6 +59,57 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.starter_strategy(game_state)
 
         game_state.submit_turn()
+
+
+    def start(self):
+        """
+        Start the parsing loop.
+        After starting the algo, it will wait until it recieves information from the game
+        engine, proccess this information, and respond if needed to take it's turn.
+        The algo continues this loop until it recieves the "End" turn message from the game.
+        """
+        debug_write(BANNER_TEXT)
+
+        while True:
+            # Note: Python blocks and hangs on stdin. Can cause issues if connections aren't setup properly and may need to
+            # manually kill this Python program.
+            game_state_string = get_command()
+            if "replaySave" in game_state_string:
+                """
+                This means this must be the config file. So, load in the config file as a json and add it to your AlgoStrategy class.
+                """
+                parsed_config = json.loads(game_state_string)
+                self.on_game_start(parsed_config)
+            elif "turnInfo" in game_state_string:
+                state = json.loads(game_state_string)
+                stateType = int(state.get("turnInfo")[0])
+                if stateType == 0:
+                    """
+                    This is the game turn game state message. Algo must now print to stdout 2 lines, one for build phase one for
+                    deploy phase. Printing is handled by the provided functions.
+                    """
+                    self.on_turn(game_state_string)
+                elif stateType == 1:
+                    """
+                    If stateType == 1, this game_state_string string represents a single frame of an action phase
+                    """
+                    self.on_action_frame(game_state_string)
+                elif stateType == 2:
+                    """
+                    This is the end game message. This means the game is over so break and finish the program.
+                    """
+                    debug_write("Got end state, game over. Stopping algo.")
+                    break
+                else:
+                    """
+                    Something is wrong? Received an incorrect or improperly formatted string.
+                    """
+                    debug_write("Got unexpected string with turnInfo: {}".format(game_state_string))
+            else:
+                """
+                Something is wrong? Received an incorrect or improperly formatted string.
+                """
+                debug_write("Got unexpected string : {}".format(game_state_string))
 
 
     """
